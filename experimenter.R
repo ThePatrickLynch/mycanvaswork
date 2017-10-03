70944a32c501d79b70cca1935e145a1526379c21
ThePatrickLynch <p.lynch@hull.ac.uk>
2017-10-03 09:12:04 +0100

    bits

--- experimenter.R
+++ experimenter.R
@@ -14,17 +14,23 @@ outFile <- "requestsprocessed"
 remove <- c(1,3:5,8:9,16,22,24:27)
 rec.skip <- 0                   # starting point
 rec.chunksize <- 750    # number of records in each chunk use 1000000 or less for Excel
-
+i=1
 
 thisoutFile <- capture.output(cat(outFile, i, ".csv"))    # new file for each chunk
 canvasrequest <- read.table(myFile, sep='\t', na.strings="\\N", quote='"', fill=TRUE, skip=rec.skip, nrows=rec.chunksize) 
 canvasrequest <- canvasrequest[ -c(remove)] # strip unwanted
 
+colnames(canvasrequest) = c("timestamp", "user_id", "course_id", "quiz_id", "discussion_id", "conversation_id", 
+                        "assignment_id", "url", "user_agent", "remote_ip","interaction_micros", "web_application_controller", 
+                        "web_applicaiton_action", "web_application_context_type","real_user_id")
+
+
+write.table(canvasrequest, thisoutFile, sep=",", col.names = FALSE, append = TRUE)
+
+
 
 
 file2 <- read.table("user_dim.txt", sep='\t', na.strings="\\N", quote='"', fill=TRUE)
-
-
 
 
 
--- squish file.R
+++ squish file.R
@@ -13,25 +13,28 @@ outFile <- "requestsprocessed"
 
 remove <- c(1,3:5,8:9,16,22,24:27)
 rec.skip <- 0                   # starting point
-rec.chunksize <- 750    # number of records in each chunk use 1000000 or less for Excel
-
-
+rec.chunksize <- 750000    # number of records in each chunk use 1000000 or less for Excel
 
 i <- 1
 
-for (i in 1:2){
+for (i in 1:2000){
   # need to go enough times to catch whole file
   # Read next chunk
 
   thisoutFile <- capture.output(cat(outFile, i, ".csv"))    # new file for each chunk
   canvasrequest <- read.table(myFile, sep='\t', na.strings="\\N", quote='"', fill=TRUE, skip=rec.skip, nrows=rec.chunksize) 
   
-  #colnames(canvasrequest) = c("timestamp", "user_id", "course_id", "quiz_id", "discussion_id", "conversation_id", 
-  #                        "assignment_id", "url", "user_agent", "remote_ip","interaction_micros", "web_application_controller", 
-  #                        "web_applicaiton_action", "web_application_context_type","real_user_id")
+
   
+  colnames(canvasrequest) = c("timestamp", "user_id", "course_id", "quiz_id", "discussion_id", "conversation_id", 
+                          "assignment_id", "url", "user_agent", "remote_ip","interaction_micros", "web_application_controller", 
+                          "web_applicaiton_action", "web_application_context_type", "real_user_id")
+ 
+
   canvasrequest <- canvasrequest[ -c(remove)] # strip unwanted
   
+  
+### timestamp is getting trashed
   
   write.table(canvasrequest, thisoutFile, sep=",", col.names = FALSE, append = TRUE)
   rec.skip <- rec.skip + rec.chunksize + 1
