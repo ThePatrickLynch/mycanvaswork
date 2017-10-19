4d8bb91cddbeaa7177a74ff09273b372c4cf44cc
ThePatrickLynch <p.lynch@hull.ac.uk>
2017-10-19 15:46:23 +0100

    bits&bobs

--- mysqlloadcsv.R
+++ mysqlloadcsv.R
@@ -1,55 +1,125 @@
 
-setwd("D:/canvas/unpackedFiles/csv")
+#######################
+# Takes csvs and loads them into tables of the same name in canvas_data
+############################
 
+
+setwd("D:/canvas/unpackedFiles/csv")
+library("gdata", lib.loc="d:/rlibraries")
 library('RMySQL')
 
+# DB Connection
 con <- dbConnect(MySQL(),
                  user="root", password="",
                  dbname="canvas_data", host="localhost")
-
-
 on.exit(dbDisconnect(con))
 
 
+###################################
+# Functions
+###################################
+
 writetoMySQL <- function(inFile) {
-    tblName <- substr(inFile, 1, nchar(inFile)-4) # lose the .txt
+  # deals with files that are small enough to be written direct to MYSQL table
+  
+  tblName <- substr(inFile, 1, nchar(inFile)-4) # lose the .txt
+  
+  # whilst testing
+  qry <- paste('DELETE FROM', tblName)
+  rslt <- dbGetQuery(con, qry)
+  
+  inFile <- substr(inFile, 1, nchar(inFile)-4) # lose the .txt
+  inFile <- paste(inFile, ".csv", sep="")
+  
+  dbWriteTable(con, tblName, inFile, append= TRUE, row.names = F)
+  
+  # print a completed date time -this could be written to a table  
+  fsize <- humanReadable(file.info(inFile)$size)
+  print(paste(tblName, "(", fsize, ") - complete", format(Sys.time(), "%a %d-%m-%Y %X")), sep = "", quote = F)
+  
+  return()
+}
+
+writetough <- function(inFile) {
+  # reads big files in bits and appends them to MySql table
+  rec.skip <- 0
+  rec.chunksize <- 10000
+
+  tblName <- substr(inFile, 1, nchar(inFile)-4) # lose the txt
+  this.File <- paste('d:/canvas/unpackedFiles/csv/', tblName, ".csv", sep="") 
+  print(this.File)
+  
+  # whilst testing clear table first
+  qry <- paste('DELETE FROM', tblName)
+  rslt <- dbGetQuery(con, qry)
+
+  for (pass in 1:5) {
+    # loop through the chunks until EOF
+    # check eof ?  
     
-    # whilst testing
-    qry <- paste('DELETE FROM', tblName)
-    rsl <- dbGetQuery(con, qry)
-    
-    inFile <- substr(inFile, 1, nchar(inFile)-4) # lose the .txt
-    inFile <- paste(inFile, ".csv", sep="")
-    
-    dbWriteTable(con, tblName, inFile, append= TRUE, row.names = F)
-    
-    this.time <- format(Sys.time(), "a %b %d %Y %X")
+    this.Table <- read.table(this.File,sep=',', na.strings="NA", quote='"', fill=TRUE, skip=rec.skip, nrows=rec.chunksize, header = T) 
+    #dbWriteTable(con, tblName, this.Table, append= TRUE, row.names = F)
+    print(rec.skip)
+    #print(rec.chunksize)
+    rec.skip <- rec.skip + rec.chunksize 
+  }
+  
+  # print a completed date time -this could be written to a table  
+  fsize <- humanReadable(file.info(this.File)$size)
+
+  print(paste(tblName, "(", fsize, ") - complete", format(Sys.time(), "%a %d-%m-%Y %X")), sep = "", quote = F)
 
   return()
 }
 
-# remove ones which won't just upload
-excl = c("conversation_message_dim.txt",
-         "file_dim.txt", 
-         "group_dim.txt", 
-         "module_completion_requirement_dim.txt",
-         "module_dim.txt",
-         "module_item_dim.txt",
-         "module_prerequisite_dim.txt",
-         "module_progression_completion_requirement_dim.txt",
-         "module_progression_dim.txt",
-         "score_dim"
-         )
 
+######################################
+# Main bit
+######################################
+
+
+# list of files which won't just upload as too big
+excl = c(  
+        "file_dim.txt" ,
+         "submission_comment_dim.txt",
+        "conversation_message_dim.txt", 
+        "module_item_fact.txt"
+)
+
+
+# removes the big stuff
 allFiles <- allFiles [! allFiles %in% excl]
 
-
+#write easier ones direct to MySQL database using function
 rsl <- lapply(allFiles, writetoMySQL)
 
+# now deal with the big files
+print('######################################')
+print('           Complicated ones')
+print('######################################')
+
+
+# need to just drop score_fact as no table yet
+excl = c(
+         "file_dim.txt" ,
+         "module_item_fact.txt"
+)
+
+
+# "conversation_message_dim.txt" ,
+# "submission_comment_dim.txt",
+
+
+rsl <- lapply(excl, writetough)
 
 
 
-#dbWriteTable(con, 'conversation_message_dim.csv', name = "conversation_message_dim", append= TRUE, row.names = F)
+# hardest
+excl = c(
+  "conversation_message_dim.txt" ,
+  "submission_comment_dim.txt"
+)
+
 
 
 #===========================================
